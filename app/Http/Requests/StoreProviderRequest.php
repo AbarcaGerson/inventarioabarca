60ad9095e0cee32bbe446312ad30952a50da65ef
AbarcaGerson <geda.abarca@hotmail.com>
2023-02-28 00:24:34 -0500

    Se agrega el proyecto inventario Abarca

---
+++ app/Http/Requests/StoreProviderRequest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProviderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'name'=>'required|string|max:255',
+            'email'=>'required|email|string|max:200|unique:providers',
+            'ruc_number'=>'required|string|max:11|min:11|unique:providers',
+            'address'=>'nullable|string|max:255',
+            'phone'=>'required|string|max:50|max:12|min:9|unique:providers',
+        ];
+    }
+
+    public function messages(){
+        return [
+            'name.required'=>'Este campo es requerido.',
+            'name.string'=>'El valor no es correcto.',
+            'name.max'=>'Solo se permite 50 caracteres.',
+
+            'email.required'=>'Este campo es requerido.',
+            'email.email'=>'No es un correo electrónico.',
+            'email.string'=>'El valor no es correcto.',
+            'email.max'=>'Solo se permiten 255 caracteres.',
+            'email.unique'=>'Ya se encuentra registrado.',
+
+            'ruc_number.required'=>'Este campo es requerido.',
+            'ruc_number.string'=>'El valor no es correcto.',
+            'ruc_number.max'=>'Solo se permiten 11 caracteres.',
+            'ruc_number.min'=>'Se requiere de 11 caracteres.',
+            'ruc_number.unique'=>'Ya se encuentra registrado.',
+
+            'address.max'=>'Solo se permiten 255 caracteres.',
+            'address.string'=>'El valor no es correcto.',
+
+            'phone.required'=>'Este campo es requerido.',
+            'phone.string'=>'El valor no es correcto.',
+            'phone.max'=>'Solo se permiten 12 caracteres',
+            'phone.min'=>'Se requiere de 9 caracteres.',
+            'phone.unique'=>'Ya se encuentra registrado.',
+        ];
+    }
+}
